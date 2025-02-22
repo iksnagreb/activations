b562c83830167fff4dc7e1985b1152f9036d161e
Christoph Berganski <christoph.berganski@gmail.com>
2025-02-22 15:27:28 +0100

    Add per-category breakdown of resource utilization metrics

--- metrics.py
+++ metrics.py
@@ -13,6 +13,22 @@ if __name__ == "__main__":
     with open(params["report"]) as file:
         # Load the JSON formatted report
         report = pd.read_json(file, orient="index")
+
+    # Collect metrics breakdown as dictionary by categories
+    breakdown = {}
+    # First collect the metrics breakdown by categories as configured
+    for category, f in params["breakdown"].items():
+        # Filter and count the number of occurrences per category
+        count = len(report.filter(regex=f, axis="rows"))
+        # Filter and summarize according to the category-specified rule
+        summary = report.filter(regex=f, axis="rows").sum()
+        # Insert into the nested breakdown dictionary
+        breakdown[category] = {"COUNT": count, **summary.to_dict()}
+    # Dump the per-category breakdown to its own report file
+    with open("breakdown.yaml", "w") as file:
+        # Dumpy python dictionary to YAML file
+        yaml.safe_dump(breakdown, file)
+
     # Filter the reported rows according to some regex filter rule
     report = report.filter(regex=params["filter"], axis="rows")
     # Generate a summary of the total resources
