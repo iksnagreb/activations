b843975e46f9fe1289ef59027d0893d131925e07
Christoph Berganski <christoph.berganski@gmail.com>
2025-05-04 23:07:22 +0000

    dvc: commit experiment 0f8e5263bdc85b7bdb324fac6b9d6e55d85cab5d8d68e6d520fb32cde68e349c

--- benchmark.sh
+++ benchmark.sh
@@ -14,7 +14,7 @@ args=(
   # Sweep smaller bit-widths actually testing the quantized activation functions
   -S model.bits="2,4,8"
   # FP or POWER_OF_TWO for both, quantizers and affine scales
-  -S model.restrict_scaling_type="FP"
+  -S model.restrict_scaling_type="FP,POWER_OF_TWO"
   # There are probably no insights to gain from comparing narrow range vs.
   # non-narrow range
   -S model.narrow_range="false"
@@ -25,11 +25,11 @@ args=(
   -S prepare.streamline="true"
   # Test thresholding (with operator fusion) vs. standalone quantizers (with
   # elementwise operations potentially in float arithmetic)
-  -S prepare.thresholds="false,true"
+  -S prepare.thresholds="false"
   # Always test with bit-width minimization for some cleaner comparisons: The
   # key comparison is standalone vs. thresholds
   -S build.minimize_bit_width="true"
 )
 
 # Fill the experiment queue spanning the whole grid configured above
-dvc exp run --queue "${args[@]}"
+dvc exp run --queue "${args[@]}" --force
